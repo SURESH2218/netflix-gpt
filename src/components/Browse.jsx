e06184b5ecaabf6b1a474054a09fe1163fb7a1b1
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-01-17 18:45:47 +0530

    dispatched movieLists into redux Store

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,21 +1,8 @@
-import React, { useEffect } from "react";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
 
 const Browse = () => {
-  const getNowPlayingMovie = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovie();
-  }, []);
-
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
